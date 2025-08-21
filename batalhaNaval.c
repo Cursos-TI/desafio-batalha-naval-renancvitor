aec4a529345ed523d3cff04249c8b47cbc4b9e92
Renan C. Vitor <98927761+renancvitor@users.noreply.github.com>
2025-08-21 16:14:19 +0000

    feat: adiciona conslusão desafio nível aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,52 +1,81 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
+#define INDICE 10
+#define TAMANHO_NAVIO 3
+
 void game()
 {
-    int tabuleiro[10][10];
-    char indiceColuna[10] = {'A'};
+    int tabuleiro[LINHAS][COLUNAS];
+    char indiceColuna[INDICE] = {'A'};
 
     // Estrutura limpa/zerada do tabuleiro (somente com 0)
-    printf(" ~*~ ### TABULEIRO DE BATALHA NAVAL ### ~*~\n");
-    for (int i = 0; i < 10; i++)
+    printf(" <*~ ### TABULEIRO DE BATALHA NAVAL ### ~*>\n");
+    for (int i = 0; i < LINHAS; i++)
     {
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNAS; j++)
         {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // Trecho do cabeçalho
-    printf("  "); // Espaço para alinha cabeçalho
-    for (int j = 0; j < 10; j++)
+    // Trecho do índice
+    printf("  "); // Espaço para alinhar índice
+    for (int j = 0; j < INDICE; j++)
     {
         printf("   %c", 'A' + j);
     }
     printf("\n");
 
-    // Posicionamento do navioH
-    int navioH[3] = {3, 3, 3};
+    // Posicionamento do navioHorizontal
+    int navioHorizontal[TAMANHO_NAVIO] = {3, 3, 3};
     int linha = 2, coluna = 1;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
     {
-        tabuleiro[linha][coluna + i] = navioH[i];
+        if (coluna + i < 10) // Validação para confirmar posicionamento dentro do tabuleiro
+            tabuleiro[linha][coluna + i] = navioHorizontal[i];
     }
 
-    // Posicionamento do navioV
-    int navioV[3] = {3, 3, 3};
+    // Posicionamento do navioVertical
+    int navioVertical[TAMANHO_NAVIO] = {3, 3, 3};
     linha = 5;
     coluna = 7;
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
     {
-        tabuleiro[linha + i][coluna] = navioV[i];
+        if (linha + i < 10) // Validação para confirmar posicionamento dentro do tabuleiro
+            tabuleiro[linha + i][coluna] = navioVertical[i];
+    }
+
+    // Posicionamento do navioDiagonalEsquerdaSul
+    int navioDiagonalEsquerdaSul[TAMANHO_NAVIO] = {3, 3, 3};
+    linha = 5;
+    coluna = 2;
+
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
+    {
+        if (linha + i < 10 && coluna - i >= 0) // Validação para confirmar posicionamento dentro do tabuleiro
+            tabuleiro[linha + i][coluna - i] = navioDiagonalEsquerdaSul[i];
+    }
+
+    // Posicionamento do navioDiagonalDireitaNorte
+    int navioDiagonalDireitaNorte[TAMANHO_NAVIO] = {3, 3, 3};
+    linha = 2;
+    coluna = 7;
+
+    for (int i = 0; i < TAMANHO_NAVIO; i++)
+    {
+        if (linha - i >= 0 && coluna + i < 10) // Validação para confirmar posicionamento dentro do tabuleiro
+            tabuleiro[linha - i][coluna + i] = navioDiagonalDireitaNorte[i];
     }
 
     // Tabuleiro montado e atualizado com os navios H e V
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < LINHAS; i++)
     {
         printf("%2d ", 1 + i); // Cria as linhas numeradas de 1 a i (no caso é 10) - 2d alinha à direita
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < COLUNAS; j++)
         {
             printf(" %2d ", tabuleiro[i][j]);
         }
